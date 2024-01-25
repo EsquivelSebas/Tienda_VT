b6c910626acfac79bba1df6ecced41f2eeb21ad8
Sebastian <Sebawtisn@gmail.com>
2024-01-25 00:01:06 -0600

    Portafolio_Diseño

---
+++ Tienda_VT/src/test/java/com/tienda_VT/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_VT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
