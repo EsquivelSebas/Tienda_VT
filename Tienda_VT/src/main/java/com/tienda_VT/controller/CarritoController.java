9793108e19a9a1730e0fa1e5639a9e993bbde24c
Sebastian <Sebawtisn@gmail.com>
2024-04-26 04:29:57 -0600

    carrito

---
+++ Tienda_VT/src/main/java/com/tienda_VT/controller/CarritoController.java
@@ -0,0 +1,60 @@
+
+package com.tienda_VT.controller;
+
+import com.tienda_VT.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class CarritoController {
+
+    @Autowired
+    private ProductoService productoService;
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/carrito/listado")
+    public String listado(Model model) {
+        var lista = itemService.gets();
+        var carritoTotal=0;
+        for (Item i : lista) {
+            carritoTotal+=(i.getCantidad() * i.getPrecio());
+
+        }
+        model.addAttribute("items",lista);
+        model.addAttribute("carritoTotal",carritoTotal);
+        
+
+        return "/carrito/listado";
+    }
+
+    @GetMapping("/carrito/agregar/{idProducto}")
+    public ModelAndView agregar(Item item, Model model) {
+        Item item2 = itemService.get(item);
+        if (item2 == null) {
+            Producto p = productoService.getProducto(item);
+            item2 = new Item(p);
+        }
+        itemService.save(item2);
+        var lista = itemService.gets();
+        var totalCarrito = 0;
+        var totalCompra = 0;
+        for (Item i : lista) {
+            totalCarrito += i.getCantidad();
+            totalCompra += (i.getCantidad() * i.getPrecio());
+        }
+        model.addAttribute("listaitems", lista);
+        model.addAttribute("listaTotal", totalCarrito);
+        model.addAttribute("carritoTotal", totalCompra);
+        return new ModelAndView("/carrito/fragmentos :: verCarrito");
+    }
+    @GetMapping("/carrito/eliminar/{idProducto}")
+    public String eliminar (Item item){
+        itemService.delete(item);
+        return "redirect:/carrito/listado";
+    }
+}
